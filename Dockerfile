e698beb703948639efb1c1ee70512ecd4153b054
Ariukasq1 <Ariuka.sq@gmail.com>
2023-05-24 17:08:45 +0800

    nestjs + prisma + mysql =project added

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node:18
+RUN mkdir -p /app
+WORKDIR /app
+
+ADD . ./
+RUN npm install
---
+++ src/book/book.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class BookService {
+  constructor(private prisma: PrismaService) {}
+  create(createBookDto: CreateBookDto) {
+    return 'This action adds a new book';
+  }
+
+  findAll() {
+    return this.prisma.book.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.book.findUnique({
+      where: { id },
+    });
+  }
+
+  update(id: number, updateBookDto: UpdateBookDto) {
+    return `This action updates a #${id} book`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} book`;
+  }
+}
